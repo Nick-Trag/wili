30456f7de878fada53074ae796a316c165891872
Nick-Trag <ntragkas@csd.auth.gr>
2024-01-02 21:54:48 +0200

    Used a question mark icon for image-less items

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  // Next TODO: Dummy data (enought to fill the screen). Will do the UI of the home page first and then move on to the other pages
   List<WishlistItem> items = [
     WishlistItem(name: "Computer", category: "Tech", price: 250, link: "https://www.example.com/", image: "assets/4060ti.jpg"),
     WishlistItem(name: "Camera", category: "Tech"),
