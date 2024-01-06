ee34125d39dd324ba5fa7f7311f7db36db5feb23
Nick-Trag <ntragkas@csd.auth.gr>
2024-01-06 18:29:41 +0200

    Added a new TODO (moving on to detail view)

--- lib/main.dart
+++ lib/main.dart
@@ -33,22 +33,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  // Next TODO: All the possible values and errors (no items, no pics, no prices, etc.). Then make it look nice (TEXT FORMATTING TOO (eg the prices)). Will do the UI of the home page first and then move on to the other pages.
-  // List<WishlistItem> items = [
-  //   WishlistItem(name: "Computer", category: "Tech", price: 250, link: "https://www.example.com/", image: "assets/4060ti.jpg"),
-  //   WishlistItem(name: "Camera", category: "Tech", price: 400),
-  //   WishlistItem(name: "Mouse", category: "Tech"),
-  //   WishlistItem(name: "Lens", category: "Tech"),
-  //   WishlistItem(name: "Microwave", category: "Home"),
-  //   WishlistItem(name: "Phone", category: "Tech"),
-  //   WishlistItem(name: "Watch", category: "Misc."),
-  //   WishlistItem(name: "Basketball", category: "Hobby"),
-  //   WishlistItem(name: "Couch", category: "Home"),
-  //   WishlistItem(name: "Pokemon Plush", category: "Misc."),
-  //   WishlistItem(name: "Hoodie", category: "Clothes"),
-  //   WishlistItem(name: "Shoes", category: "Clothes"),
-  // ];
-  List<WishlistItem> items = [];
+  // Next TODO: other pages (Detail view first)
+  List<WishlistItem> items = [
+    WishlistItem(name: "Computer", category: "Tech", price: 250, link: "https://www.example.com/", image: "assets/4060ti.jpg"),
+    WishlistItem(name: "Camera", category: "Tech", price: 400),
+    WishlistItem(name: "Mouse", category: "Tech"),
+    WishlistItem(name: "Lens", category: "Tech"),
+    WishlistItem(name: "Microwave", category: "Home"),
+    WishlistItem(name: "Phone", category: "Tech"),
+    WishlistItem(name: "Watch", category: "Misc."),
+    WishlistItem(name: "Basketball", category: "Hobby"),
+    WishlistItem(name: "Couch", category: "Home"),
+    WishlistItem(name: "Pokemon Plush", category: "Misc."),
+    WishlistItem(name: "Hoodie", category: "Clothes"),
+    WishlistItem(name: "Shoes", category: "Clothes"),
+  ];
+  // List<WishlistItem> items = [];
 
   void _incrementCounter() {
     setState(() {
