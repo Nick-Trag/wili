6a0ec7e3640d5ee4e2d471c7a924223c9fa75cb1
Nick-Trag <ntragkas@csd.auth.gr>
2024-01-07 15:24:00 +0200

    Started implementing the ViewItem page

---
+++ lib/pages/view_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:wili/classes/item.dart';
+
+class ViewItemWidget extends StatelessWidget {
+  const ViewItemWidget({super.key, required this.item});
+
+  final WishlistItem item;
+
+  @override
+  Widget build(BuildContext  context) {
+    return const Text("temp");
+  }
+}
