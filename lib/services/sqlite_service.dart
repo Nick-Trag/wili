cb62e9c39e65fde301a94d507add38f76e9e5e3b
Nick-Trag <ntragkas@csd.auth.gr>
2024-02-11 14:30:07 +0200

    Updated the SQL to add the categories table

--- lib/services/sqlite_service.dart
+++ lib/services/sqlite_service.dart
@@ -8,16 +8,23 @@ class SQLiteService {
     return openDatabase(
       join(path, 'database.db'),
       onCreate: (database, version) async {
+        await database.execute("CREATE TABLE Categories("
+          "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+          "name TEXT NOT NULL"
+        ")");
         await database.execute("CREATE TABLE Items("
           "id INTEGER PRIMARY KEY AUTOINCREMENT,"
           "name TEXT NOT NULL,"
-          "category TEXT NOT NULL,"
+          "category_id INTEGER NOT NULL,"
           "price REAL NOT NULL DEFAULT 0,"
           "purchased INTEGER NOT NULL DEFAULT 0," // Actually a bool
           "note TEXT NOT NULL DEFAULT '',"
           "quantity INTEGER NOT NULL DEFAULT 1,"
           "link TEXT NOT NULL DEFAULT '',"
-          "image TEXT NOT NULL DEFAULT ''"
+          "image TEXT NOT NULL DEFAULT '',"
+          "FOREIGN KEY (category_id)"
+            "REFERENCES Categories (id)"
+            "ON DELETE SET DEFAULT" // Currently there is no default, so this should not even work
         ")"); // TODO: Full schema + categories table + categories as foreign key
       },
       version: 0,
