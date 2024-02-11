c887d9c246910b320e7f24786baf8d40c74de219
Nick-Trag <ntragkas@csd.auth.gr>
2024-02-11 02:48:51 +0200

    Added a toMap method. Will need to incorporate the ID as well

--- lib/classes/item.dart
+++ lib/classes/item.dart
@@ -9,4 +9,18 @@ class WishlistItem {
   String image; // local path to image
 
   WishlistItem({required this.name, required this.category, this.price = 0, this.purchased = false, this.note = "", this.quantity = 1, this.link = "", this.image = ""});
+
+  // TODO: + id
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'category': category,
+      'price': price,
+      'purchased': purchased,
+      'note': note,
+      'quantity': quantity,
+      'link': link,
+      'image': image,
+    };
+  }
 }
