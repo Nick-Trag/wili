8eedfbda494f3213f7e5a5ba4f969c5a08ecc321
Nick-Trag <ntragkas@csd.auth.gr>
2024-03-05 01:47:28 +0200

    Created skeleton for editing categories (will be significantly changed)

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wili/providers/item_provider.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({super.key});
@@ -11,7 +13,52 @@ class CategoriesWidget extends StatelessWidget {
         title: const Text('Categories'),
         centerTitle: true,
       ),
-      body: const Placeholder(), // TODO: All categories, in cards, with an edit button (opens modal for new name) and delete button (opens modal for confirmation)
+      body: Consumer<ItemProvider>(
+        builder: (context, provider, child) {
+          if (provider.categories.isEmpty) { // This should never be the case
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.not_interested),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 100),
+                  child: const Text(
+                    "There are no categories.",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            );
+          }
+          else {
+            return ListView.builder(
+              itemCount: provider.categories.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(provider.categories.values.elementAt(index)),
+                    trailing: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.edit),
+                        ),
+                        Icon(Icons.delete),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ), // TODO: All categories, in cards, with an edit button (opens modal for new name) and delete button (opens modal for confirmation)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // TODO: Open a modal to create a new category. Also, careful, might be hiding some action buttons
+        tooltip: 'Add a new category',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
