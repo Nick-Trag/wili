6c8d8f5a79cdbd64e72bb0a72508fc61f4fe9af1
Nick-Trag <ntragkas@csd.auth.gr>
2024-03-04 20:42:08 +0200

    Got a large part of the UI for the settings page done

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -12,7 +12,38 @@ class SettingsWidget extends StatelessWidget {
         title: const Text("Settings"),
         centerTitle: true,
       ),
-      body: const Placeholder(),
+      body: Stack(
+        children: [
+          ListView(
+            children: <Widget>[
+              InkWell(
+                child: const ListTile(
+                  title: Text("Currency"),
+                  subtitle: Text("€"),
+                ),
+                onTap: () {
+                  // TODO: Show modal with a TextFormField that makes lets users type in their currency symbol or name
+                },
+              ),
+              const Divider(height: 0),
+              InkWell(
+                child: const ListTile(
+                  title: Text('Edit categories'),
+                ),
+                onTap: () {
+                  // TODO: Show all categories and let users delete and rename them (always keeping one alive)
+                },
+              ),
+              const Divider(height: 0),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            alignment: Alignment.bottomCenter,
+            child: const Text("Made with ♥ from Greece"),
+          ),
+        ],
+      ),
     );
   }
 
