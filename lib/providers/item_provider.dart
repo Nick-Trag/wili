8bbefdd8e39e0aa2c9b2f87660ce5d044e16d4a8
Nick-Trag <ntragkas@csd.auth.gr>
2024-03-10 20:06:29 +0200

    Removed unused imports

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -1,9 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:wili/classes/item.dart';
 import 'package:wili/services/sqlite_service.dart';
 
