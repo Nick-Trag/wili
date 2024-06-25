d7a83f8e9f4bf233ce3a21b8e039de0bd5f575e5
Nick-Trag <ntragkas@csd.auth.gr>
2024-06-26 01:18:35 +0300

    Working logic for showing total prices. Need to improve the UI

--- lib/widgets/list_widget.dart
+++ lib/widgets/list_widget.dart
@@ -32,29 +32,61 @@ class ListWidget extends StatelessWidget {
       );
     }
     else { // TODO: Add total prices (Perhaps total not purchased + total purchased) (take quantity into account)
-      return ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          if (Provider.of<SettingsProvider>(context).moveToBot && index != items.length - 1 && !items[index].purchased && items[index + 1].purchased) {
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      double totalPrice = 0;
+      double notPurchasedPrice = 0;
+      for (WishlistItem item in items) {
+        double itemPrice = item.quantity * item.price;
+        if (!item.purchased) {
+          notPurchasedPrice += itemPrice;
+        }
+        totalPrice += itemPrice;
+      }
+
+      return Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("Price (not purchased): $notPurchasedPrice", textAlign: TextAlign.left,),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("Total price: $totalPrice", textAlign: TextAlign.right,),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                if (Provider.of<SettingsProvider>(context).moveToBot && index != items.length - 1 && !items[index].purchased && items[index + 1].purchased) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                        child: ItemCardWidget(item: items[index], categoryName: categories[items[index].category]!),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 12.0),
+                        child: Divider(height: 0),
+                      ),
+                    ],
+                  );
+                }
+                return Padding(
+                  // The last item gets considerably more padding on the bottom, in order for the floating action button to not hide any item's price
+                  padding: index == items.length - 1 ? const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 55.0): const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
                   child: ItemCardWidget(item: items[index], categoryName: categories[items[index].category]!),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 12.0),
-                  child: Divider(height: 0),
-                ),
-              ],
-            );
-          }
-          return Padding(
-            // The last item gets considerably more padding on the bottom, in order for the floating action button to not hide any item's price
-            padding: index == items.length - 1 ? const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 55.0): const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-            child: ItemCardWidget(item: items[index], categoryName: categories[items[index].category]!),
-          );
-        },
+                );
+              },
+            ),
+          ),
+        ],
       );
     }
   }
